b2c37653d2e84bc22fd6fe87e25eaf77edfb0ffd
David Romero <adavidromero@gmail.com>
2016-05-06 11:38:07 -0600

    second commit

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { set; get; }
+        public string LastName { set; get; }
+        public string FirstMidName { set; get; }
+        public DateTime EnrollmentDate { set; get; }
+
+        public virtual ICollection<Enrollment> Enrollments { set; get; }
+    }
+}
